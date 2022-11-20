d474f90acb9d699eb8731386da7bfe467dccb851
Ismaelp96 <ismael6991@live.com>
2022-11-20 15:18:46 -0300

    events

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-// const inputName = document.getElementById("exampleInputName");
-// const form = document.getElementById("form-user-create");
-// const inputBirth = document.getElementById("exampleInputBirth");
-// const inputCountry = document.getElementById("exampleInputCountry");
-// const inputEmail = document.getElementById("exampleInputEmail");
-// const inputPassoword = document.getElementById("exampleInputPassword");
-// const photoFile = document.getElementById("exampleInputFile");
-// const admin = document.getElementById("exampleInputAdmin");
-
-// console.log(inputName);
-
 const fields = document.querySelectorAll("#form-user-create [name]");
 
-fields.forEach((field, index) => {
-  if (field.name == "gender") {
-    if (field.checked) {
-      console.log("sim", field);
-    }
-  } else {
-    console.log("Não");
-  }
-});
+const user = {};
+
+document
+  .getElementById("form-user-create")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    fields.forEach((field, index) => {
+      if (field.name == "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else {
+        user[field.name] = field.value;
+      }
+    });
+  });
