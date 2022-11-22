76cf7f4244b28a4844e53769d164baab5f6d1010
Ismaelp96 <ismael6991@live.com>
2022-11-21 23:20:08 -0300

    validando formulários

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ class User {
    constructor(name, gender, birth, country, email, password, photo, admin) {
       this._name = name;
       this._gender = gender;
+      this._birth = birth;
       this._country = country;
       this._email = email;
-      this._birth = birth;
       this._password = password;
       this._photo = photo;
       this._admin = admin;
@@ -14,32 +14,72 @@ class User {
    get name() {
       return this._name;
    }
+
    get gender() {
       return this._gender;
    }
+
    get birth() {
       return this._birth;
    }
+
    get country() {
       return this._country;
    }
+
    get email() {
       return this._email;
    }
+
    get password() {
       return this._password;
    }
+
    get photo() {
       return this._photo;
    }
+
    get admin() {
       return this._admin;
    }
+
    get register() {
       return this._register;
    }
 
+   // set name(value) {
+   //    this._name = value;
+   // }
+
+   // set gender(value) {
+   //    this._gender = value;
+   // }
+
+   // set birth(value) {
+   //    this._birth = value;
+   // }
+
+   // set country(value) {
+   //    this._country = value;
+   // }
+
+   // set email(value) {
+   //    this._email = value;
+   // }
+
+   // set admin(value) {
+   //    this._admin = value;
+   // }
+
    set photo(value) {
       this._photo = value;
    }
+
+   // set password(value) {
+   //    this._password = value;
+   // }
+
+   // set register(value) {
+   //    this._register = value;
+   // }
 }
