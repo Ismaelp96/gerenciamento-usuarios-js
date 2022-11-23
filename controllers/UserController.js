db27bb46b64dda24f1f11e6f1d20e09eb8697718
Ismaelp96 <ismael6991@live.com>
2022-11-22 23:10:27 -0300

    colocando event no botão excluir

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -171,7 +171,7 @@ class UserController {
      <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">
        Editar
      </button>
-     <button type="button" class="btn btn-danger btn-xs btn-flat">
+     <button type="button" class="btn btn-danger dtn-delete btn-xs btn-flat">
        Excluir
      </button>
    </td>
@@ -185,6 +185,12 @@ class UserController {
   }
 
   addEventsTr(tr) {
+    tr.querySelector(".dtn-delete").addEventListener("click", (e) => {
+      if (confirm("Deseja realmente excluir?")) {
+        tr.remove();
+        this.updateCount();
+      }
+    });
     tr.querySelector(".btn-edit").addEventListener("click", (e) => {
       let json = JSON.parse(tr.dataset.user);
 
